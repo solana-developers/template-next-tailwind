5ee8ac310f3442ceab380ac1be98d037617418bc
Bram Borggreve <beeman@beeman.nl>
2024-09-11 19:58:55 +0100

    chore: initial commit

---
+++ src/components/solana/solana-provider.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import {WalletError} from '@solana/wallet-adapter-base'
+import {ConnectionProvider, WalletProvider,} from '@solana/wallet-adapter-react'
+import {WalletModalProvider} from '@solana/wallet-adapter-react-ui'
+import dynamic from 'next/dynamic'
+import {ReactNode, useCallback, useMemo} from 'react'
+import {useCluster} from '../cluster/cluster-data-access'
+
+require('@solana/wallet-adapter-react-ui/styles.css')
+
+export const WalletButton = dynamic(async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton, {
+  ssr: false,
+})
+
+export function SolanaProvider({ children }: { children: ReactNode }) {
+  const { cluster } = useCluster()
+  const endpoint = useMemo(() => cluster.endpoint, [cluster])
+  const onError = useCallback((error: WalletError) => {
+    console.error(error)
+  }, [])
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[]} onError={onError} autoConnect={true}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
+
